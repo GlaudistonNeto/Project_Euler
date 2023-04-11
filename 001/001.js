c29404526b160716f72bc9023bbc703badb1dced
Glaudiston <glaudistonson@gmail.com>
2023-04-11 20:12:31 -0300

    Euler Project ✨feature: Solving Project Euler challenge 001

--- 001/001.js
+++ 001/001.js
@@ -1,22 +1,36 @@
-let num = 1000;
-
-let sum = 0;
-
-console.time('Method 1')
-for (let i =0; i < num; i++) {
-  if (i % 3 === 0 || i % 5 === 0)
-    sum += i;
+function sumMultiples(n) {
+  const num3 = Math.floor((n - 1) / 3);
+  const num5 = Math.floor((n - 1) / 5);
+  const num15 = Math.floor((n - 1) / 15);
+  return 3 * num3 * (num3 + 1) / 2 +
+         5 * num5 * (num5 + 1) / 2 -
+         15 * num15 * (num15 + 1) / 2;
 }
 
-console.log(sum);
-console.timeEnd('Method 1')
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-console.time('Method 2')
+let t;
+let n = [];
 
-num--
-const no3 = 3 * Math.floor(num/3) * (Math.floor(num/3) +1) / 2
-const no5 = 5 * Math.floor(num/5) * (Math.floor(num/5) +1) / 2
-const no15 = 15 * Math.floor(num/15) * (Math.floor(num/15) +1) / 2
+rl.on('line', (input) => {
+  if (!t) {
+    t = parseInt(input);
+  } else {
+    n.push(parseInt(input));
+    if (n.length === t) {
+      rl.close();
+    }
+  }
+}).on('close', () => {
+  for (let i = 0; i < t; i++) {
+    console.log(sumMultiples(n[i]));
+  }
+  process.exit(0);
+});
 
-console.log(no3 + no5- no15);
-console.timeEnd('Method 2')
+
+// it fucking fails in test 3 and 4
