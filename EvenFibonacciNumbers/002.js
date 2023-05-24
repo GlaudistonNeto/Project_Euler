5c734854eaf0fd4bbb530cf20a161e28179245aa
Glaudiston <glaudistonson@gmail.com>
2023-05-24 00:09:17 -0300

    Changed from a for statement to while so it worked

--- EvenFibonacciNumbers/002.js
+++ EvenFibonacciNumbers/002.js
@@ -1,24 +1,47 @@
 
-const x = 1;
-const y = 2;
-sum = 0;
+// const x = 1;
+// const y = 2;
+// sum = 0;
 
-function fibonacci() {
-  for (let y = 1; y <= 4000000; y++){
-    if (y != 2){
-      y++
+// function fibonacci() {
+//   for (let y = 1; y <= 4000000; y++){
+//     if (y != 2){
+//       y++
+//     }
+//     sum = x + y;
+//     console.log(sum);
+//     // only counting even numbers
+//   }
+
+//   return sum;
+// } // finish it
+
+// fibonacci();
+
+// // tudo
+// // function sequence() {
+// //   fibonacci() + y;
+// // }
+
+const sumEvenFibonacci = (limit) => {
+  let sum = 0;
+  let prev = 1;
+  let current = 2;
+
+  while (current <= limit) {
+    if (current % 2 === 0) {
+      sum += current;
     }
-    sum = x + y;
-    console.log(sum);
-    // only counting even numbers
+
+    const next = prev + current;
+    prev = current;
+    current = next;
   }
 
   return sum;
-} // finish it
+};
 
-fibonacci();
+const limit = 4000000;
+const sum = sumEvenFibonacci(limit);
+console.log(sum);
 
-// tudo
-// function sequence() {
-//   fibonacci() + y;
-// }
