7d1e915208f8a98a8e2f30c5de14296af91ab7a0
Glaudiston <glaudistonson@gmail.com>
2023-05-24 14:56:09 -0300

    ✅ The Largest Prime Factor is...

---
+++ LargestPrimeFactor/003.js
@@ -0,0 +1,16 @@
+function largestPrimeFactor(number) {
+  let factor = 2;
+  while (factor <= number) {
+    if (number % factor === 0) {
+      number /= factor;
+    } else {
+      factor++;
+    }
+  }
+  return factor;
+}
+
+const number = 600851475143;
+const largestFactor = largestPrimeFactor(number);
+
+console.log("The largest prime factor of", number, "is", largestFactor);
