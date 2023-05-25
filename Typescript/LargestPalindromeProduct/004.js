fbc0766522740c1dd4fb2d275aac8079882a403d
Glaudiston <glaudistonson@gmail.com>
2023-05-25 19:48:19 -0300

    ✨ still trying challenge four

---
+++ Typescript/LargestPalindromeProduct/004.js
@@ -0,0 +1,24 @@
+"use strict";
+function isPalindrome(num) {
+    const numStr = num.toString();
+    const reversedStr = numStr.split('').reverse().join('');
+    return numStr === reversedStr;
+}
+function findLargestPalindrome() {
+    let largestPalindrome = 0;
+    for (let i = 999; i >= 100; i--) {
+        for (let j = i; j >= 100; j--) {
+            const product = i * j;
+            if (product <= largestPalindrome) {
+                // No need to continue if the product is smaller or equal to the largest palindrome found so far
+                break;
+            }
+            if (isPalindrome(product) && product > largestPalindrome) {
+                largestPalindrome = product;
+            }
+        }
+    }
+    return largestPalindrome;
+}
+const largestPalindrome = findLargestPalindrome();
+console.log(`The largest palindrome made from the product of two 3-digit numbers is ${largestPalindrome}.`);
